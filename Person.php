ff8bf22f32c6d22c149d8300fd7318db2b38ed5f
zarinatenorio <zarit-tt@hotmail.com>
2017-01-29 13:06:52 -0500

    Add files via upload

---
+++ Person.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+class Person {
+	public $name;
+
+	public function __construct ($name) {
+		$this->name = $name;
+	}
+	public function getName () {
+
+		return $this->name;
+     }
+
+
+}
+
+
+?>
